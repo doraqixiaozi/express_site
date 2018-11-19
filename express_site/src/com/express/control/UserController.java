33a2e5d67dc95d60f08a1d512f112a5276155d18
doraqixiaozi <862965251@qq.com>
2018-11-20 00:18:47 +0800

    简单整合

--- express_site/src/com/express/control/UserController.java
+++ express_site/src/com/express/control/UserController.java
@@ -1,8 +1,21 @@
 package com.express.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.express.pojo.LoginResult;
+import com.express.pojo.User;
 
 @Controller
 public class UserController {
-
+@RequestMapping("/login")
+	public @ResponseBody LoginResult login(@RequestBody User user) {
+	    System.out.println(user);
+	
+	
+		return null;
+		
+	}
 }
