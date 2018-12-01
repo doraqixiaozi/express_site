5ea7d2a734e402b403da8e0249a9b8460a10d4e3
doraqixiaozi <862965251@qq.com>
2018-12-01 23:29:11 +0800

    增加了websocket基础配置

--- express_site/src/com/express/control/UserController.java
+++ express_site/src/com/express/control/UserController.java
@@ -3,13 +3,13 @@ package com.express.control;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -25,7 +25,7 @@ import com.express.pojo.SignUpForm;
 import com.express.pojo.User;
 import com.express.service.MessageService;
 import com.express.service.UserService;
-import com.sun.net.httpserver.Authenticator.Success;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class UserController {
@@ -33,9 +33,11 @@ public class UserController {
 	private UserService userService;
 	@Resource
 	private MessageService messageService;
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@RequestMapping("/login")
-	public @ResponseBody LoginResult login(@Validated @RequestBody LoginForm loginForm, BindingResult result,HttpSession session) {
+	public @ResponseBody LoginResult login(@Validated @RequestBody LoginForm loginForm, BindingResult result,
+			HttpSession session) {
 		List<String> errors = new ArrayList<>();
 		LoginResult login = new LoginResult();
 		if (result.hasErrors()) {
@@ -56,8 +58,8 @@ public class UserController {
 	}
 
 	@RequestMapping("/signin")
-	public @ResponseBody String signup(@Validated SignUpForm user, BindingResult result,MultipartFile avatr
-,HttpServletRequest request) {
+	public @ResponseBody String signup(@Validated SignUpForm user, BindingResult result, MultipartFile avatr,
+			HttpServletRequest request) {
 		if (result.hasErrors()) {
 			List<ObjectError> list = result.getAllErrors();
 			for (ObjectError objectError : list) {
@@ -65,60 +67,79 @@ public class UserController {
 			}
 			return "error";
 		}
-		//重复校验
+		// 重复校验
 		List<String> similarEmail = userService.getSimilarEmail(user.getEmail());
 		for (String string : similarEmail) {
 			if (user.getEmail().equals(string)) {
 				return "user has exist";
 			}
-		}				
+		}
 		try {
 			try {
-				userService.doSignup(user,avatr,request);
+				userService.doSignup(user, avatr, request);
 			} catch (IllegalStateException e) {
-				// TODO 自动生成的 catch 块
 				e.printStackTrace();
 				return "unkown error";
 			} catch (IOException e) {
-				// TODO 自动生成的 catch 块
 				e.printStackTrace();
 				return "not a image";
 			}
 		} catch (DataAccessException e) {
 			e.printStackTrace();
 			return "user has exist";
-		}		
+		}
 		return "success";
 	}
 
 	@RequestMapping("/similarEmail")
-	public @ResponseBody List<String> getSimilarEmail(@RequestBody SignUpForm form) {
-		String email = form.getEmail();
-		form = null;
+	public @ResponseBody List<String> getSimilarEmail(@RequestBody String json_email) {
+		Map readValue=null;
+		try {
+			readValue = mapper.readValue(json_email, Map.class);
+		} catch (IOException e) {
+			return null;		
+		}
+		String email = (String) readValue.get("email");
 		List<String> similarEmail = userService.getSimilarEmail(email);
 		return similarEmail;
 	}
+
 	@RequestMapping("/searchfriend")
-	public @ResponseBody User searchfriend(@RequestBody SignUpForm form) {
-		String email = form.getEmail();
-		form=null;
+	public @ResponseBody User searchfriend(@RequestBody String json_email) {
+		Map readValue=null;
+		try {
+			readValue = mapper.readValue(json_email, Map.class);
+		} catch (IOException e) {
+			return null;		
+		}
+		String email = (String) readValue.get("email");
 		User user = userService.searchfriend(email);
 		return user;
 	}
-	
+
 	@RequestMapping("/addfriend")
-	public @ResponseBody String addfriend(@RequestBody User user,HttpSession session) {
-     Integer f_id=user.getId();
-     user = (User) session.getAttribute("user");
-     if (user == null) {
-		return "please login first";
+	public @ResponseBody String addfriend(@RequestBody String json_id, HttpSession session){
+		if (json_id == null||json_id.trim().equals("")) {
+			return "please put the userid";
+		}
+		Map readValue;		
+			try {
+				readValue = mapper.readValue(json_id, Map.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return "unkown error";
+			}	
+		Integer f_id = Integer.parseInt((String) readValue.get("id"));
+		User user = (User) session.getAttribute("user");
+		if (user == null) {
+			return "please login first";
+		}
+		Integer s_id = user.getId();
+		if (s_id == f_id) {
+			return "you can not add yourself as friend";
+		}
+		userService.makeFriends(s_id, f_id);
+		return "success";
 	}
-     Integer s_id=user.getId();     
-     if (s_id==f_id) {
-		return "you can not add yourself as friend";
-	}
-     userService.makeFriends(s_id,f_id);
-     return "success";
-	}
-	
+
 }
---
+++ express_site/src/com/express/websocket/MyWebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.express.websocket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * Component注解告诉SpringMVC该类是一个SpringIOC容器下管理的类
+ * 其实@Controller, @Service, @Repository是@Component的细化
+ */
+@Configuration  
+@EnableWebMvc
+@EnableWebSocket
+public class MyWebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+    @Autowired
+    MyWebSocketHandler handler;
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        //添加websocket处理器，添加握手拦截器
+        webSocketHandlerRegistry.addHandler(handler, "/websocket.action").addInterceptors(new MyHandShakeInterceptor());
+
+        //添加websocket处理器，添加握手拦截器
+        webSocketHandlerRegistry.addHandler(handler, "/websocket/sockjs.action").addInterceptors(new MyHandShakeInterceptor()).withSockJS();
+    }
+}
